2fbcb74d6ac82b085eb7a3a0294541713bf6e5ee
Арина <161595187+GagarinaArina745@users.noreply.github.com>
2025-06-13 02:28:56 +0300

    1

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import requests
+import io
+import os
 
-# Загрузка обученной модели
-model = joblib.load('mobile_price_model.pkl')
+# Укажите здесь путь к локальному файлу или URL модели
+MODEL_PATH_OR_URL = 'mobile_price_model.pkl'  # или 'https://example.com/path/to/model.pkl'
+
+def load_model(path_or_url):
+    if os.path.isfile(path_or_url):
+        # Загружаем из файла
+        return joblib.load(path_or_url)
+    else:
+        # Предполагаем, что это URL
+        response = requests.get(path_or_url)
+        response.raise_for_status()  # Проверка успешности запроса
+        return joblib.load(io.BytesIO(response.content))
+
+# Загружаем модель один раз при запуске приложения
+model = load_model(MODEL_PATH_OR_URL)
 
 st.title("Определение ценового диапазона мобильного телефона")
 
